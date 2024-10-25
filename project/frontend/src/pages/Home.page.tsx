33ef3225ed3f9606e53135810c4280ff9e9b8bde
ashik <gub221902131@gmail.com>
2024-10-25 18:47:08 +0600

    Your commit message

--- project/frontend/src/pages/Home.page.tsx
+++ project/frontend/src/pages/Home.page.tsx
@@ -1,13 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import useApp from "../hooks/useApp.hook";
-import { useEffect } from "react";
 
 export default function HomePage({ appLoad }: { appLoad: boolean }) {
   const { user } = useApp();
   const navigate = useNavigate();
-  useEffect(() => {
-    if (!user?.id) navigate("/auth");
-  }, []);
+
+  if (!user?.id) navigate("/auth");
+
   return (
     <>
       {appLoad && <div className="bg-primary h-fit" />}
